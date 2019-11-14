2867c6d5b055466f6894e3d8cf9258015ec3a1a9
jolson615 <jolson615@gmail.com>
2019-11-14 10:40:41 -0500

    adds typescript

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import Navbar from './components/Navbar.js'
-import SchoolCard from './components/SchoolCard.js'
-import data from './nycSchoolEnrollmentData.js'
+// Be sure to uncomment these components and the data before you use them!
+// import SchoolCard from './components/SchoolCard.js'
+// import data from './nycSchoolEnrollmentData.js'
 import './App.css';
 
 const App = () => {
   const component = new React.Component()
-  
+
   component.render = () => {
     return (
     <div className="App">
       <Navbar/>
       <div className="container">
         <div className="row">
-          
+
         </div>
       </div>
     </div>
   )}
-  
+
   return component
 }
 
